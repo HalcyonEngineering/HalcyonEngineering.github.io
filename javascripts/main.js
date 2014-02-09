3d9e048b235133e260a0ac2ca61fb16b317d43df
Andy Wang <andy_wang347@hotmail.com>
2014-02-08 19:07:12 -0800

    meeting 9 mintues + site update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
 console.log('This would be the main JS file.');
+$( index ).ready( function() {
+            var $body = $('h6'); //Cache this for performance
+
+            var setBodyScale = function() {
+                var scaleSource = $body.width(),
+                    scaleFactor = 0.35,                     
+                    maxScale = 600,
+                    minScale = 30; //Tweak these values to taste
+
+                var fontSize = scaleSource * scaleFactor; //Multiply the width of the body by the scaling factor:
+
+                if (fontSize > maxScale) fontSize = maxScale;
+                if (fontSize < minScale) fontSize = minScale; //Enforce the minimum and maximums
+
+                $('h6').css('font-size', fontSize + '%');
+            }
+
+            $(window).resize(function(){
+                setBodyScale();
+            });
+
+            //Fire it when the page first loads:
+            setBodyScale();
+        });
